c84318394f9d7d53a0ee61a756574e8278b7868e
Sushma Kovvuri <50438716+kovvurisushma@users.noreply.github.com>
2024-03-17 19:11:46 +0530

    Update and rename Level Order Traversal.txt to Level Order Traversal.cpp

--- Trees/Level Order Traversal.cpp
+++ Trees/Level Order Traversal.cpp
@@ -1,7 +1,9 @@
+/*
 Problem: Print Level Order Traversal of given Binary Tree
 link: https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+*/
 
-Solution:
+// Solution:
 
 void levelOrderTraversal(Node* root) {
   if(!root) return;
@@ -17,6 +19,7 @@ void levelOrderTraversal(Node* root) {
   return;
 }
 
+/*
 - In this approach all the nodes are printed in single line with space seperated
 
 Approach in case we have to differentiate levels
@@ -25,8 +28,9 @@ Approach in case we have to differentiate levels
 - and while traversing through the queue, check if the poped element is null, if its null then pop null from queue and add it at the end;
 - this may result in infinite loop, if we add null without checking if q is empty or not
 - so check if queue is empty or not, if it is not empty then add the null
+*/
 
-Solution: 
+// Solution: 
 
 void levelOrderTraversal(Node* root) {
   if(!root) return;
